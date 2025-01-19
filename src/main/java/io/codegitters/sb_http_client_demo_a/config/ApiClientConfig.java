944e53df7b3163ed63a5497b9c182c90f89f599c
hrosasnutz <hrosasnutz.1991@gmail.com>
2025-01-19 08:21:21 -0500

    feat: update web client config
    Signed-off-by: hrosasnutz <hrosasnutz.1991@gmail.com>

--- src/main/java/io/codegitters/sb_http_client_demo_a/config/ApiClientConfig.java
+++ src/main/java/io/codegitters/sb_http_client_demo_a/config/ApiClientConfig.java
@@ -1,20 +1,45 @@
 package io.codegitters.sb_http_client_demo_a.config;
 
 import io.codegitters.sb_http_client_demo_a.client.JsonPlaceHolderApiClient;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.ReactiveAdapterRegistry;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class ApiClientConfig {
     
     @Bean
+    WebClient.Builder apiWebClientBuilder() {
+        return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(HttpClient.create()
+                        .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 10000)
+                        .doOnConnected(cnn -> cnn
+                                .addHandlerFirst(new ReadTimeoutHandler(10, TimeUnit.SECONDS))
+                                .addHandlerLast(new WriteTimeoutHandler(10, TimeUnit.SECONDS)))
+                        .responseTimeout(Duration.ofSeconds(5))));
+    }
+    
+    @Bean
     WebClientAdapter jsonPlaceHolderWebClientAdapter() {
-        return WebClientAdapter.create(WebClient
-                .create("https://jsonplaceholder.typicode.com/"));
+        return WebClientAdapter.create(apiWebClientBuilder()
+                .baseUrl("https://jsonplaceholder.typicode.com/")
+                .build());
     }
     
     @Bean
@@ -24,4 +49,15 @@ public class ApiClientConfig {
                 .build()
                 .createClient(JsonPlaceHolderApiClient.class);
     }
+    
+    @Bean
+    Customizer<Resilience4JCircuitBreakerFactory> defaultConfig() {
+        return factory -> factory.configureDefault(id -> 
+                new Resilience4JConfigBuilder(id)
+                        .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+                        .timeLimiterConfig(TimeLimiterConfig.custom()
+                                .timeoutDuration(Duration.ofSeconds(5)).build())
+                        .build());
+    }
+            
 }
